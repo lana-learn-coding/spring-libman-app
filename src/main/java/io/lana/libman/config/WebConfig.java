ecd38cf6878d7b90b3673e352cc1a4719492fe71
Lana <sitdownrightnow2552@gmail.com>
2022-06-06 22:08:43 +0700

    chore: re-enable mvc auto config

--- src/main/java/io/lana/libman/config/WebConfig.java
+++ src/main/java/io/lana/libman/config/WebConfig.java
@@ -3,11 +3,9 @@ package io.lana.libman.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig {
     @Bean

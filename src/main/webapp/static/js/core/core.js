b5e265e04490478731f24e5a81e72f90a9384730
Lana <sitdownrightnow2552@gmail.com>
2022-06-19 00:54:05 +0700

    feat: rewrite core setup using unpoly complier

--- src/main/webapp/static/js/core/core.js
+++ src/main/webapp/static/js/core/core.js
@@ -6,50 +6,52 @@
     queue.push(func);
   };
   document.addEventListener('DOMContentLoaded', () => {
-    // Feather icon initial
-    feather.replace();
-
-    // Auto active with query for unpoly
-    document.querySelectorAll('[up-nav=include-query]').forEach(e => {
-      e.querySelectorAll('a').forEach(a => {
-        const href = a.href || a.getAttribute('up-href');
-        if (!href) return;
-        const alias = a.getAttribute('up-alias') || '';
-        a.setAttribute('up-alias', alias + ' ' + href + '?*');
-      });
-    });
-
-    up.on('up:fragment:inserted', (e) => {
-      feather.replace();
-      renderToast();
-      _hyperscript.processNode(e.target);
-      hideWrapper();
-    });
-
-    function renderToast() {
-      const toastContainer = document.getElementById('notify-container');
-      const toastList = document.querySelectorAll('#notify-hungry .toast');
-      if (toastList.length && toastContainer) {
-        toastList.forEach(toast => {
-          toastContainer.appendChild(toast);
-          bootstrap.Toast.getOrCreateInstance(toast).show();
-        });
-      }
-    }
-
-    function hideWrapper() {
-      const loader = document.querySelector('.loader-wrapper');
-      if (loader) {
-        loader.style.transition = 'opacity 1.6s';
-        setTimeout(() => {
-          loader.style.opacity = '0';
-          setTimeout(() => {
-            loader.remove();
-          }, 1500);
-        }, 100);
-      }
-    }
-
     while (queue.length) queue.shift().call();
   });
 })();
+
+// Initial setup
+defer(() => {
+  up.fragment.config.runScripts = true;
+
+  up.on('up:fragment:inserted', (e) => {
+    feather.replace();
+    _hyperscript.processNode(e.target);
+  });
+
+  up.macro('[up-nav=include-query]', (e) => {
+    e.querySelectorAll('a').forEach(a => {
+      const href = a.href || a.getAttribute('up-href');
+      if (!href) return;
+      const alias = a.getAttribute('up-alias') || '';
+      a.setAttribute('up-alias', alias + ' ' + href + '?*');
+    });
+  });
+
+  up.compiler('#notify-container', (toastContainer) => {
+    const toastList = document.querySelectorAll('#notify-hungry .toast');
+    if (toastList.length && toastContainer) {
+      toastList.forEach(toast => {
+        toastContainer.appendChild(toast);
+        bootstrap.Toast.getOrCreateInstance(toast).show();
+      });
+    }
+  });
+
+  up.compiler('.loader-wrapper', (loader) => {
+    if (loader) {
+      loader.style.transition = 'opacity 1.6s';
+      let timeoutRemove = null;
+      const timeOutOpacity = setTimeout(() => {
+        loader.style.opacity = '0';
+        timeoutRemove = setTimeout(() => {
+          loader.remove();
+        }, 1300);
+      }, 100);
+      return () => {
+        clearTimeout(timeoutRemove);
+        clearTimeout(timeOutOpacity);
+      };
+    }
+  });
+});

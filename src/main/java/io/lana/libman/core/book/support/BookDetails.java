c32ff76170bd408ce1aed547b1f475cfc4582299
Lana <sitdownrightnow2552@gmail.com>
2022-06-22 23:24:26 +0700

    fix: remove id requirement from book details interface (id of different entities has different means)

--- src/main/java/io/lana/libman/core/book/support/BookDetails.java
+++ src/main/java/io/lana/libman/core/book/support/BookDetails.java
@@ -3,8 +3,6 @@ package io.lana.libman.core.book.support;
 import java.util.Set;
 
 public interface BookDetails {
-    String getId();
-
     String getTitle();
 
     String getAuthorName();
--- src/main/java/io/lana/libman/core/book/support/SimpleBookDetail.java
+++ src/main/java/io/lana/libman/core/book/support/SimpleBookDetail.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -25,19 +24,5 @@ public class SimpleBookDetail implements BookDetails {
     @Override
     public String getGenresName() {
         return String.join(",", genresSet);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-        BookDetails other = (BookDetails) obj;
-        return Objects.equals(getId(), other.getId());
     }
 }

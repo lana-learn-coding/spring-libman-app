4419758ae149420275bdd002a9087396f7d03103
Lana <sitdownrightnow2552@gmail.com>
2022-07-03 22:51:37 +0700

    fix: using 'and' when search reader with email

--- src/main/java/io/lana/libman/core/reader/ReaderRepo.java
+++ src/main/java/io/lana/libman/core/reader/ReaderRepo.java
@@ -13,7 +13,7 @@ public interface ReaderRepo extends PagingAndSortingRepository<Reader, String> {
 
     @Query(value = "select r from Reader r left join fetch r.account a " +
             "where (:query is null or lower(a.phone) like lower(:query) or lower(a.firstName) like lower(:query) or lower(a.lastName) like lower(:query)) " +
-            "or (:email is null or lower(a.email) like lower(:email))",
+            "and (:email is null or lower(a.email) like lower(:email))",
             countQuery = "select count(r.id) from Reader r left join r.account " +
                     "where (:query is null or lower(r.account.phone) like lower(:query) or lower(r.account.firstName) like lower(:query) or lower(r.account.lastName) like lower(:query)) " +
                     "and (:email is null or lower(r.account.email) like lower(:email))")
